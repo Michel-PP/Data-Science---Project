5d491d2dad3e54db2b4addb73d9141c831f2b1d6
Michel-PP <84421995+Michel-PP@users.noreply.github.com>
2021-05-21 16:08:23 +0200

    Add files via upload

--- project.R
+++ project.R
@@ -12,6 +12,10 @@ library(scales)
 library(readxl)
 library(lubridate)
 library(shiny)
+library(plyr)
+library(dplyr)
+library(collapsibleTree)
+
 
 as_decade <- function(value){ return(value - value %% 10) }
 
@@ -20,12 +24,28 @@ ABitLessThanAMillionSong <- read_xlsx("C:\\Users\\Michel\\Documents\\UCR\\2nd Se
 
 hot100 <- read.csv("C:\\Users\\Michel\\Documents\\UCR\\2nd Semester\\Data Science\\Codes\\Project\\Hot Stuff.csv")
 
-genre <- read.csv("C:\\Users\\Michel\\Documents\\UCR\\2nd Semester\\Data Science\\Codes\\Project\\genres4.csv", sep = ";") %>% slice(1:1046)
+genre <- read.csv("C:\\Users\\Michel\\Documents\\UCR\\2nd Semester\\Data Science\\Codes\\Project\\Data-Science---Project\\genres4.csv",
+                  sep = ";") %>% slice(1:1046)
 
-medium <- read.csv2("C:\\Users\\Michel\\Documents\\UCR\\2nd Semester\\Data Science\\Codes\\Project\\medium.csv")
+medium <- read.csv2("C:\\Users\\Michel\\Documents\\UCR\\2nd Semester\\Data Science\\Codes\\Project\\Data-Science---Project\\medium3.csv")
+
+medium_50 <- read.csv2("C:\\Users\\Michel\\Documents\\UCR\\2nd Semester\\Data Science\\Codes\\Project\\Data-Science---Project\\medium_50.csv")
+
+medium_60 <- read.csv2("C:\\Users\\Michel\\Documents\\UCR\\2nd Semester\\Data Science\\Codes\\Project\\Data-Science---Project\\medium_60.csv")
+
+medium_70 <- read.csv2("C:\\Users\\Michel\\Documents\\UCR\\2nd Semester\\Data Science\\Codes\\Project\\Data-Science---Project\\medium_70.csv")
+
+medium_80 <- read.csv2("C:\\Users\\Michel\\Documents\\UCR\\2nd Semester\\Data Science\\Codes\\Project\\Data-Science---Project\\medium_80.csv")
+
+medium_90 <- read.csv2("C:\\Users\\Michel\\Documents\\UCR\\2nd Semester\\Data Science\\Codes\\Project\\Data-Science---Project\\medium_90.csv")
+
+medium_00 <- read.csv2("C:\\Users\\Michel\\Documents\\UCR\\2nd Semester\\Data Science\\Codes\\Project\\Data-Science---Project\\medium_00.csv")
+
+medium_10 <- read.csv2("C:\\Users\\Michel\\Documents\\UCR\\2nd Semester\\Data Science\\Codes\\Project\\Data-Science---Project\\medium_10.csv")
+
 
 hot100_dates <- hot100 %>% 
-  rename(title = Song) %>% 
+  dplyr::rename(title = Song) %>% 
   select(-SongID) %>% 
   mutate(WeekID = as_date(WeekID, format = "%m/%d/%Y"),
          year = year(WeekID),
@@ -58,27 +78,30 @@ data <- inner_join(ABitLessThanAMillionSong,hot100_dates) %>%
          singerSongwriter = grepl("singer-songwriter", spotify_genre),
          genre = NA
   )
-
-data$genre[data$rock == TRUE] <- "Rock"
-data$genre[data$pop == TRUE] <- "Pop"
-data$genre[data$country == TRUE] <- "Country"
-data$genre[data$soul == TRUE] <- "Soul"
-data$genre[data$mellowGold == TRUE] <- "MellowGold"
-data$genre[data$folk == TRUE] <- "Folk"
-data$genre[data$rap == TRUE] <- "Rap"
-data$genre[data$adultStandarts == TRUE] <- "Adult Standarts"
-data$genre[data$motown == TRUE] <- "Motown"
-data$genre[data$hipHop == TRUE] <- "Hip Hop"
-data$genre[data$quietStorm == TRUE] <- "Quiet Storm"
-data$genre[data$funk == TRUE] <- "Funk"
-data$genre[data$disco == TRUE] <- "Disco"
-data$genre[data$urbanContemporary == TRUE] <- "Urban Contemporary"
-data$genre[data$rAndB == TRUE] <- "R&B"
-data$genre[data$lounge == TRUE] <- "Lounge"
-data$genre[data$jazz == TRUE] <- "Jazz"
-data$genre[data$metal == TRUE] <- "Metal"
-data$genre[data$easyListening == TRUE] <- "Easy Listening"
 data$genre[data$singerSongwriter == TRUE] <- "Singer-Songwriter"
+data$genre[data$easyListening == TRUE] <- "Easy Listening"
+data$genre[data$metal == TRUE] <- "Metal"
+data$genre[data$jazz == TRUE] <- "Jazz"
+data$genre[data$lounge == TRUE] <- "Lounge"
+data$genre[data$rAndB == TRUE] <- "R&B"
+data$genre[data$urbanContemporary == TRUE] <- "Urban Contemporary"
+data$genre[data$disco == TRUE] <- "Disco"
+data$genre[data$funk == TRUE] <- "Funk"
+data$genre[data$quietStorm == TRUE] <- "Quiet Storm"
+data$genre[data$hipHop == TRUE] <- "Hip Hop"
+data$genre[data$motown == TRUE] <- "Motown"
+data$genre[data$adultStandarts == TRUE] <- "Adult Standarts"
+data$genre[data$rap == TRUE] <- "Rap"
+data$genre[data$folk == TRUE] <- "Folk"
+data$genre[data$mellowGold == TRUE] <- "MellowGold"
+data$genre[data$soul == TRUE] <- "Soul"
+data$genre[data$country == TRUE] <- "Country"
+data$genre[data$pop == TRUE] <- "Pop"
+data$genre[data$rock == TRUE] <- "Rock"
+
+
+
+
 
 data_50 <- data %>% 
   subset(WeekID >= "1950-01-01" & WeekID <= "1959-12-31")
@@ -102,13 +125,13 @@ data_00 <- data %>%
 data_10 <- data %>% 
   subset(WeekID >= "2010-01-01" & WeekID <= "2021-12-31")
 
-  medium_50 <- data_50 %>% slice(1:1000)
-  medium_60 <- data_60 %>% slice(1:1000)
-  medium_70 <- data_70 %>% slice(1:1000)
-  medium_80 <- data_80 %>% slice(1:1000)
-  medium_90 <- data_90 %>% slice(1:1000)
-  medium_00 <- data_00 %>% slice(1:1000)
-  medium_10 <- data_10 %>% slice(1:1000)
+  medium_50 <- medium_50 %>% sample_n(10000)
+  medium_60 <- medium_60 %>% sample_n(10000)
+  medium_70 <- medium_70 %>% sample_n(10000)
+  medium_80 <- medium_80 %>% sample_n(10000)
+  medium_90 <- medium_90 %>%  sample_n(10000)
+  medium_00 <- medium_00 %>%  sample_n(10000)
+  medium_10 <- medium_10 %>% sample_n(10000)
   
 
   medium <- full_join(medium,medium_10)
@@ -137,7 +160,14 @@ medium %>%
   mutate(WeekID = dmy(WeekID)) 
 
 write.csv2(table(df) %>%  as_tibble() %>% arrange(desc(n)), file = "C:\\Users\\Michel\\Documents\\UCR\\2nd Semester\\Data Science\\Codes\\Project\\genres3.csv", row.names = FALSE)
-write.csv2(medium, file = "C:\\Users\\Michel\\Documents\\UCR\\2nd Semester\\Data Science\\Codes\\Project\\medium.csv", row.names = FALSE)
+write.csv2(medium, file = "C:\\Users\\Michel\\Documents\\UCR\\2nd Semester\\Data Science\\Codes\\Project\\Data-Science---Project\\medium4.csv", row.names = FALSE)
+write.csv2(medium_00, file = "C:\\Users\\Michel\\Documents\\UCR\\2nd Semester\\Data Science\\Codes\\Project\\Data-Science---Project\\medium_00.csv", row.names = FALSE)
+write.csv2(medium_10, file = "C:\\Users\\Michel\\Documents\\UCR\\2nd Semester\\Data Science\\Codes\\Project\\Data-Science---Project\\medium_10.csv", row.names = FALSE)
+write.csv2(medium_50, file = "C:\\Users\\Michel\\Documents\\UCR\\2nd Semester\\Data Science\\Codes\\Project\\Data-Science---Project\\medium_50.csv", row.names = FALSE)
+write.csv2(medium_60, file = "C:\\Users\\Michel\\Documents\\UCR\\2nd Semester\\Data Science\\Codes\\Project\\Data-Science---Project\\medium_60.csv", row.names = FALSE)
+write.csv2(medium_70, file = "C:\\Users\\Michel\\Documents\\UCR\\2nd Semester\\Data Science\\Codes\\Project\\Data-Science---Project\\medium_70.csv", row.names = FALSE)
+write.csv2(medium_80, file = "C:\\Users\\Michel\\Documents\\UCR\\2nd Semester\\Data Science\\Codes\\Project\\Data-Science---Project\\medium_80.csv", row.names = FALSE)
+write.csv2(medium_90, file = "C:\\Users\\Michel\\Documents\\UCR\\2nd Semester\\Data Science\\Codes\\Project\\Data-Science---Project\\medium_90.csv", row.names = FALSE)
 
 
 ```{r, echo=FALSE}
@@ -153,3 +183,78 @@ data %>%
   filter(decade == "1950") %>% 
   ggplot(aes(decade, fill = genre)) +
   geom_bar(position = "fill") + theme_classic()
+
+
+data %>% 
+  group_by(genre) %>% 
+  summarise((n = n())) %>% view()
+
+collapsibleTree(
+  tiny,
+  hierarchy = c("decade", "genre", "Weeks.on.Chart"),
+  width = 700,
+  zoomable = TRUE
+)
+
+
+tiny <- medium %>% sample_n(10000)
+
+
+
+```{r summary, echo=FALSE}
+
+
+
+if (reactive({input$categoryChoice}) == "spotify_track_duration_ms"){
+  summary(lm(spotify_track_duration_ms ~ Peak.Position, medium))
+}
+
+if (reactive({input$categoryChoice}) == "tempo"){
+  summary(lm(tempo ~ Peak.Position, medium))
+}
+
+if (reactive({input$categoryChoice}) == "valence"){
+  summary(lm(valence ~ Peak.Position, medium))
+}
+
+
+if (reactive({input$categoryChoice}) == "liveness"){
+  summary(lm(liveness ~ Peak.Position, medium))
+}
+
+if (reactive({input$categoryChoice}) == "accousticness"){
+  summary(lm(accousticness ~ Peak.Position, medium))
+}
+
+if (reactive({input$categoryChoice}) == "instrumentalness"){
+  summary(lm(instrumentalness ~ Peak.Position, medium))
+}
+
+if (reactive({input$categoryChoice}) == "speechiness"){
+  summary(lm(accousticness ~ Peak.Position, medium))
+}
+
+if (reactive({input$categoryChoice}) == "loudness"){
+  summary(lm(loudness ~ Peak.Position, medium))
+}
+
+
+if (reactive({input$categoryChoice}) == "energy"){
+  summary(lm(energy ~ Peak.Position, medium))
+}
+
+```
+```{r echo = FALSE}
+selectInput("categoryChoice", label = "Chose a category:",
+            choices = c("energy", "loudness", "speechiness", "accousticness", "instrumentalness", "liveness", "valence", "tempo",
+                        "spotify_track_duration_ms"),
+            selected = "spotify_track_duration_ms", multiple = FALSE)
+```
+
+```{r test, echo=FALSE}
+summary(lm(reactive({input$categoryChoice}) ~ Peak.Position, data = medium))
+```
+
+
+
+summary(lm(energy ~ Peak.Position, data = medium))
